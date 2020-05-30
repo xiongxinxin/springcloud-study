a4040ad2b144aa47739506e60444a2adb3f732cd
熊新新 <820046719@qq.com>
2020-05-30 19:33:57 +0800

    初始化项目

---
+++ springboot-eureka/src/test/java/com/xxx/springbooteureka/SpringbootEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxx.springbooteureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
